c72dcf85f906db9dc3fa8eaef16179b48ebf7c30
ShambaMondal <80923718+ShambaMondal@users.noreply.github.com>
2023-06-27 15:58:39 +0200

    Update BITFAM.R

--- R/BITFAM.R
+++ R/BITFAM.R
@@ -32,58 +32,76 @@ BITFAM <- function(data, species, interseted_TF = NA, scATAC_obj = NA,ncores, it
   }
   
   All_TFs <-system.file("extdata", paste0(TF_targets_dir, "all_TFs.txt"), package = "BITFAM")
-  print(paste0("1. ALL_TFs: \n", ALL_TFs))
-  #print(All_TFs)
+  #print(paste0("1. ALL_TFs: \n", ALL_TFs))
+  print("1. ALL_TFs: ")
+  print(All_TFs)
   All_TFs <- read.table(All_TFs, stringsAsFactors = F)$V1
-  #print(All_TFs)
-  print(paste0("2. ALL_TFs: \n", ALL_TFs))
+  print("2. ALL_TFs: ")
+  print(All_TFs)
+  #print(paste0("2. ALL_TFs: \n", ALL_TFs))
   TF_used <- rownames(data)[rownames(data) %in% All_TFs]
-  #print(TF_used)
-  print(paste0("3. TF_used: \n", TF_used))
+  print("3. TF_used: ")
+  print(TF_used)
+  #print(paste0("3. TF_used: \n", TF_used))
   rownames(data) <- toupper(rownames(data))
-  #print(rownames(data))
-  print(paste0("4. rownames(data): \n", rownames(data)))
+  print("4. rownames(data): ")
+  print(rownames(data))
+  #print(paste0("4. rownames(data): \n", rownames(data)))
   if(is.na(interseted_TF)){
   }else{
     TF_used <- unique(c(TF_used, interseted_TF))
   }
-  #print(TF_used)
-  print(paste0("5. TF_used: \n", TF_used))
+  print("5. TF_used: ")
+  print(TF_used)
+  #print(paste0("5. TF_used: \n", TF_used))
   gene_list <- list()
   for(i in TF_used){
     TF_targets_path <-system.file("extdata", paste0(TF_targets_dir, i), package = "BITFAM")
-    #print(TF_targets_path)
-    print(paste0("6. i: \n", i))
-    print(paste0("7. TF_targets_path: \n", TF_targets_path))
+    print("6. i: ")
+    print(i)
+    print("7. TF_targets_path: ")
+    print(TF_targets_path)
+    #print(paste0("6. i: \n", i))
+    #print(paste0("7. TF_targets_path: \n", TF_targets_path))
     tmp_gene <- read.table(TF_targets_path, stringsAsFactors = F)
-    #print(tmp_gene)
-    print(paste0("8. tmp_gene: \n", tmp_gene))
+    print("8. tmp_gene: ")
+    print(tmp_gene)
+    #print(paste0("8. tmp_gene: \n", tmp_gene))
     tmp_gene <- toupper(tmp_gene$V1)
-    #print(tmp_gene)
-    print(paste0("9. tmp_gene: \n", tmp_gene))
+    print("9. tmp_gene: ")
+    print(tmp_gene)
+    #print(paste0("9. tmp_gene: \n", tmp_gene))
     gene_list[[which(TF_used == i)]] <- rownames(data)[rownames(data) %in% tmp_gene]
-    #print(gene_list)
-    print(paste0("10. gene_list: \n", gene_list))
+    print("10. gene_list: ")
+    print(gene_list)
+    #print(paste0("10. gene_list: \n", gene_list))
   }
 
   TF_used <- TF_used[ unlist(lapply(gene_list, length)) > 10]
-  #print(TF_used)
-  print(paste0("11. TF_used: \n", TF_used))
+  print("11. TF_used: ")
+  print(TF_used)
+  #print(paste0("11. TF_used: \n", TF_used))
   gene_list <- list()
   for(i in TF_used){
     TF_targets_path <-system.file("extdata", paste0(TF_targets_dir, i), package = "BITFAM")
-    #print(TF_targets_path)
-    print(paste0("12. i: \n", i))
-    print(paste0("13. TF_targets_path: \n", TF_targets_path))
+    print("12. i: ")
+    print(i)
+    print("13. TF_targets_path: ")
+    print(TF_targets_path)
+    #print(paste0("12. i: \n", i))
+    #print(paste0("13. TF_targets_path: \n", TF_targets_path))
     tmp_gene <- read.table(TF_targets_path, stringsAsFactors = F)
-    #print(tmp_gene)
-    print(paste0("14. tmp_gene: \n", tmp_gene))
+    print("14. tmp_gene: ")
+    print(tmp_gene)
+    #print(paste0("14. tmp_gene: \n", tmp_gene))
     tmp_gene <- toupper(tmp_gene$V1)
-    #print(tmp_gene)
-    print(paste0("15. tmp_gene: \n", tmp_gene))
+    print("15. tmp_gene: ")
+    print(tmp_gene)
+    #print(paste0("15. tmp_gene: \n", tmp_gene))
     gene_list[[which(TF_used == i)]] <- rownames(data)[rownames(data) %in% tmp_gene]
-    #print(gene_list)
-    print(paste0("16. gene_list: \n", gene_list))
+    print("16. gene_list: ")
+    print(gene_list)
+    #print(paste0("16. gene_list: \n", gene_list))
   }
   
   if(is.na(scATAC_obj)){
@@ -92,42 +110,53 @@ BITFAM <- function(data, species, interseted_TF = NA, scATAC_obj = NA,ncores, it
       gene_list[[which(TF_used == i)]] <- gene_list[[which(TF_used == i)]][gene_list[[which(TF_used == i)]] %in% BITFAM_scATAC(scATAC_obj)]
     }
   }
-  print(paste0("17. This is after the scATAC code block: \n", TF_used))
+  print("17. This is after the scATAC code block: ")
   
   X <- t(as.matrix(data))
-  #print(X)
-  print(paste0("18. X: \n", X))
+  print("18. X: ")
+  print(X)
+  #print(paste0("18. X: \n", X))
   chipseq_weight <- matrix(1, nrow = length(colnames(X)), ncol = length(TF_used))
-  #print(chipseq_weight)
-  print(paste0("19. chipseq_weight: \n", chipseq_weight))
+  print("19. chipseq_weight: ")
+  print(chipseq_weight)
+  #print(paste0("19. chipseq_weight: \n", chipseq_weight))
   for(i in 1:length(TF_used)){
     chipseq_weight[, i] <- ifelse(colnames(X) %in% gene_list[[i]], 1, 0)
-    #print(chipseq_weight)
-    print(paste0("20. i: \n", i))
-    print(paste0("21. chipseq_weight: \n", chipseq_weight))
+    print("20. i: ")
+    print(i)
+    print("21. chipseq_weight: ")
+    print(chipseq_weight)
+    #print(paste0("20. i: \n", i))
+    #print(paste0("21. chipseq_weight: \n", chipseq_weight))
   }
-  print(paste0("22. This is after the chipseq_weight code block: \n", TF_used))
+  print("22. This is after the chipseq_weight code block: ")
 
   Mask_matrix <- chipseq_weight
-  #print(Mask_matrix)
-  print(paste0("23. Mask_matrix: \n", Mask_matrix))
+  print("23. Mask_matrix: ")
+  print(Mask_matrix)
+  #print(paste0("23. Mask_matrix: \n", Mask_matrix))
   X <- t(as.matrix(data))
-  #print(X)
-  print(paste0("24. X: \n", X))
+  print("24. X: ")
+  print(X)
+  #print(paste0("24. X: \n", X))
   N <- dim(X)[1]
-  #print(N)
-  print(paste0("25. N: \n", N))
+  print("25. N: ")
+  print(N)
+  #print(paste0("25. N: \n", N))
   D <- dim(X)[2]
-  #print(D)
-  print(paste0("26. D: \n", D))
+  print("26. D: ")
+  print(D)
+  #print(paste0("26. D: \n", D))
   K <- length(TF_used)
-  #print(K)
-  print(paste0("27. K: \n", K))
+  print("27. K: ")
+  print(K)
+  #print(paste0("27. K: \n", K))
   data_to_model <- list(N = N, D = D, K = K, X = X, Mask = Mask_matrix)
-  #print(data_to_model)
-  print(paste0("28. data_to_model: \n", data_to_model))
+  print("28. data_to_model: ")
+  print(data_to_model)
+  #print(paste0("28. data_to_model: \n", data_to_model))
 
-  print(paste0("29. This is immediately before rstan run: \n", TF_used))
+  print("29. This is immediately before rstan run: ")
 
 
   library(rstan)
@@ -173,17 +202,17 @@ W[d,k] ~ normal(0, t_alpha[d, k]);
 to_vector(X) ~ normal(to_vector(Z*W'), t_tau);
 } "
   
-  print("30. pca_beta_prior has been computed \n")
+  print("30. pca_beta_prior has been computed ")
   m_beta_prior <- stan_model(model_code = pca_beta_piror)
-  print("31. m_beta_prior has been computed \n")
+  print("31. m_beta_prior has been computed ")
   suppressWarnings(fit.vb <- vb(m_beta_prior, data = data_to_model, algorithm = "meanfield",
                                   iter = iter, output_samples = 300, tol_rel_obj = tol_rel_obj))
-  print("32. fit.vb has been computed \n")
+  print("32. fit.vb has been computed ")
   BITFAM_list <- list(Model = fit.vb,
                       TF_used = TF_used,
                       Genes = rownames(data),
                       Cell_names = colnames(data))
-  print("33. BITFAM_list has been computed \n")
+  print("33. BITFAM_list has been computed ")
   return(BITFAM_list)
 }
 
